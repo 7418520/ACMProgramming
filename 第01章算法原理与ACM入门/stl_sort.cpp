8b82cba045e06b5bd30e713d643ee3e75f1c0bc6
ZHAO Jing <ceo@webturing.com>
2016-11-21 13:54:19 +0800

    part of chapter 03

---
+++ 第01章算法原理与ACM入门/stl_sort.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cstdlib>
+#include <alogrithm>
+using namespace std;
+int main(){
+	int data[10];
+	for(int i=0;i<5;i++)
+		cin>>data[i];
+	sort(data,data+5);
+	for(int i=0;i<5;i++)
+		cout<<a[i]<<endl;
+	return 0;
+
+}
+
