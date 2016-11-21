1c5eccd871a32a6886b2b3244c34e2bcc07dd2cf
lgb <liugaobing@foxmail.com>
2016-11-21 15:28:27 +0800

    更新 '第02章数据结构的基本知识/sqlist.cpp'

--- 第02章数据结构的基本知识/sqlist.cpp
+++ 第02章数据结构的基本知识/sqlist.cpp
@@ -1,1 +1,13 @@
+#include<iostream>
+#include<fstream>
+using namespace std;
+typedef struct LNode{  
+char data;
+  struct LNode *next;
+}LNode,*Sqlist;//单链表结构
+int intilist(Sqlist &L){
+L=new LNode;//单链表的初始化
+  L->next=NULL;
+  return 1;
+}
 
