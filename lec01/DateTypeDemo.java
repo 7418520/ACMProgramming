92553a60110d59257fbf8e329b7990b42d9e4628
ZHAO Jing <zj@ahstu.cc>
2017-06-28 09:20:57 +0800

    java入门第一讲

---
+++ lec01/DateTypeDemo.java
@@ -0,0 +1,11 @@
+public class  DateTypeDemo
+{
+	public static void main(String[] args)
+	{
+		int x=3;
+		System.out.println(Integer.toBinaryString(x));
+		int y=Integer.parseInt("1234");
+		char c='x';
+		System.out.println(Character.isLetter(c));
+	}
+}
