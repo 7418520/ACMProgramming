291c2cf1e6434817bb9c4d2aa2c2c289462a1c3f
ZHAO Jing <zj@ahstu.cc>
2017-06-29 23:19:41 +0800

    完美的代价，贪心算法

--- src/contest01/E.java
+++ src/contest01/E.java
@@ -1,13 +1,34 @@
 package contest01;
+
+import java.io.PrintWriter;
 import java.util.Scanner;
+import java.util.TreeSet;
 
-/**
- * Created by WebTuring on 2017-6-29.
- */
 public class E {
-    public static void main(String[] args) {
-        Scanner cin=new Scanner(System.in);
+    static Scanner cin = new Scanner(System.in);
+    static PrintWriter cout = new PrintWriter(System.out);
 
+    public static void main(String[] args) {
+        while (cin.hasNext()) {
+            int n = cin.nextInt();
+            if (n == 0) break;
+            TreeSet<Integer> set = new TreeSet<Integer>();
+            for (int i = 0; i < n; i++) set.add(cin.nextInt());
+            cout.println(set.size());
+            boolean first = true;
+            for (int i : set) {
+                if (first)
+                    first = false;
+                else
+                    cout.print(" ");
+                cout.print(i);
+            }
+            cout.println("\n");
+        }
+
+        cout.close();
         cin.close();
     }
+
+
 }
--- src/contest01/F.java
+++ src/contest01/F.java
@@ -1,13 +1,31 @@
 package contest01;
+
+import java.io.PrintWriter;
 import java.util.Scanner;
+import java.util.TreeSet;
 
-/**
- * Created by WebTuring on 2017-6-29.
- */
 public class F {
-    public static void main(String[] args) {
-        Scanner cin=new Scanner(System.in);
+    static Scanner cin = new Scanner(System.in);
+    static PrintWriter cout = new PrintWriter(System.out);
 
+
+    public static void main(String[] args) {
+        for (int n = cin.nextInt(); n-- > 0; ) {
+            if (foo(cin.next()).equals(foo(cin.next())))
+                cout.println("Yes");
+            else
+                cout.println("No");
+        }
+        cout.close();
         cin.close();
     }
+
+    private static TreeSet<Character> foo(String s) {
+        TreeSet<Character> set = new TreeSet<Character>();
+        for (char c : s.toCharArray())
+            set.add(c);
+        return set;
+    }
+
+
 }
