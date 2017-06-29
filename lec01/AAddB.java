c032d742f74a5057b6a88beac068a5507d4df048
ZHAO Jing <zj@ahstu.cc>
2017-06-29 21:14:47 +0800

    重构项目似的可以直接运行源代码~

--- lec01/AAddB.java
+++
@@ -1,9 +0,0 @@
-public class AAddB {
-    public static void main(String[] args) {
-        java.util.Scanner s = new java.util.Scanner(System.in);
-        int a = s.nextInt();
-        int b = s.nextInt();
-        System.out.println(a + b);
-        s.close();
-    }
-}
