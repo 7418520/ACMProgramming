33135f0127fde69e559b9ac34700f13902748479
ZHAO Jing <zj@ahstu.cc>
2017-07-01 09:29:23 +0800

    lec04

--- src/lec04/P1853.java
+++ src/lec04/P1853.java
@@ -1,8 +1,23 @@
-package lec04;
+package lec04am;
+
+import java.util.Stack;
 
 /**
- * Created by Administrator on 2017/7/1 0001.
- * 栈的应用:括号配对检测
+ * Created by zj on 17-7-1.
  */
 public class P1853 {
+    public static void main(String[] args) {
+        Stack<Character> stack=new Stack<>();
+        for(int i=0;i<5;i++)
+            stack.push((char)('a'+i));
+        System.out.println(stack);
+        stack.pop();
+        System.out.println(stack);
+        stack.push('x');
+        System.out.println(stack.peek());
+        stack.pop();
+        stack.pop();
+        System.out.println(stack);
+
+    }
 }
